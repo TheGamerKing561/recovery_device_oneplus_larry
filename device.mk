462337303c6833902f4a8bcb34770b9af0fec829
TheGamerKing <eyadtamer2121@gmail.com>
2026-07-16 20:21:21 +0300

    larry: Import QTI bootctrl HAL
    Co-authored-by: rjohnz <mega9c88@gmail.com>
    Signed-off-by: TheGamerKing <eyadtamer2121@gmail.com>

--- device.mk
+++ device.mk
@@ -46,11 +46,8 @@ AB_OTA_POSTINSTALL_CONFIG += \
 
 # Boot control HAL
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.0-impl \
-    android.hardware.boot@1.0-service
-
-PRODUCT_PACKAGES += \
-    bootctrl.holi
+    bootctrl.holi.recovery \
+    android.hardware.boot@1.1-impl-qti.recovery
 
 PRODUCT_PACKAGES += \
     otapreopt_script \
